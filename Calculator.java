36bee4625c521e4943c8791c1cbdc023b015d202
vedants27 <vedantss27@gmail.com>
2025-03-27 00:29:28 +0530

    Added method to compute sum of array elements.

--- Calculator.java
+++ Calculator.java
@@ -19,13 +19,22 @@ class Calculator {
             throw new ArithmeticException("Division by zero is not allowed.");
         }
         return numbers[0] / numbers[1];
+    }
         // Fibonacci sequence
     public int fibonacci(int n) {
         if (n <= 1) return n;
         return fibonacci(n - 1) + fibonacci(n - 2);
+       }
+    }
+       // Sum of elements in an array
+    public int sumArray(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
     }
 
-    }
 
 
 
