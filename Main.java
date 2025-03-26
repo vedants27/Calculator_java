b49cab8e87b37116201de9761068a50fc80faa9e
vedants27 <vedantss27@gmail.com>
2025-03-26 21:10:14 +0530

    Added menu for user interaction.

--- Main.java
+++ Main.java
@@ -14,6 +14,36 @@ class Main {
       Calculator var1 = new Calculator();
       UserInput var2 = new UserInput();
       Scanner var3 = new Scanner(System.in);
+
+      while(true) {
+      System.out.println("Select an operation:");
+      System.out.println("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Fibonacci\n6. Sum of Array\n7. Mean of Array\n0. Exit");
+      int var4 = var3.nextInt();
+
+      if (var4 == 0) {
+      System.out.println("Exiting the program.");
+      var3.close();
+      return;
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      }
+
    }
 
 }
