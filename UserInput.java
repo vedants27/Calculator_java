68f41ab6ac8f529e823652d3da4f62daa1e48bd2
vedants27 <vedantss27@gmail.com>
2025-03-27 00:04:23 +0530

    Added method to get two numbers java Copy Edit.

--- UserInput.java
+++ UserInput.java
@@ -3,5 +3,15 @@ import java.util.*;
 
 class UserInput {
     Scanner scanner = new Scanner(System.in);
+
+    // Method to get two integers from the user
+    int[] getTwoNumbers() {
+        int[] numbers = new int[2];
+        System.out.println("Enter first number:");
+        numbers[0] = scanner.nextInt();
+        System.out.println("Enter second number:");
+        numbers[1] = scanner.nextInt();
+        return numbers;
+    }
 }
 
