abc92ca1e1033b8c05064ec2ac2ecaa5f1c114dc
vedants27 <vedantss27@gmail.com>
2025-03-27 00:27:32 +0530

    Added method for division with error handling.

--- Calculator.java
+++ Calculator.java
@@ -13,6 +13,14 @@ class Calculator {
     public int multiply(int[] numbers) {
         return numbers[0] * numbers[1];
     }
+        // Division of two numbers
+    public int divide(int[] numbers) {
+        if (numbers[1] == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return numbers[0] / numbers[1];
+    }
+
 
 
 
