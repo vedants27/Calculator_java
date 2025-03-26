caaba053e06560f2c39908a42eb19d248d6d846c
vedants27 <vedantss27@gmail.com>
2025-03-26 21:11:10 +0530

    Added basic arithmetic operations.

--- Main.java
+++ Main.java
@@ -25,7 +25,34 @@ class Main {
       var3.close();
       return;
       }
-      
+      PrintStream var10000;
+         int[] var5;
+         int var7;
+         switch (var4) {
+            case 1:
+               var5 = var2.getTwoNumbers();
+               var10000 = System.out;
+               var7 = var1.add(var5);
+               var10000.println("Sum: " + var7);
+               break;
+            case 2:
+               var5 = var2.getTwoNumbers();
+               var10000 = System.out;
+               var7 = var1.subtract(var5);
+               var10000.println("Difference: " + var7);
+               break;
+            case 3:
+               var5 = var2.getTwoNumbers();
+               var10000 = System.out;
+               var7 = var1.multiply(var5);
+               var10000.println("Product: " + var7);
+               break;
+            case 4:
+               var5 = var2.getTwoNumbers();
+               var10000 = System.out;
+               var7 = var1.divide(var5);
+               var10000.println("Quotient: " + var7);
+               break;
       
       
       
